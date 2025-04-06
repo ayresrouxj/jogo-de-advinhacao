7e5e01e7b880204b4080f6dcc4438d87b3f0135c
Ayres <ayresrouxj@gmail.com>
2025-04-06 07:53:18 -0300

    aula: criando componente de letter

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import styles from './app.module.css'
 
 import { Header } from './components/Header'
 import { Tip } from './components/Tip'
+import { Letter } from './components/Letter'
 
 export function App() {
   function handleRestartGame() {
@@ -14,6 +15,12 @@ export function App() {
         <Header current={5} max={10} onRestart={handleRestartGame} />
 
         <Tip tip="Linguagem de programação dinâmica" />
+
+        <Letter value="R" />
+        <Letter value="" />
+        <Letter value="" />
+        <Letter value="C" />
+        <Letter value="T" />
       </main>
     </div>
   )
